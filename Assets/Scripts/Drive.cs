d2c759119143fed31550190c8c4f4182251caa78
DevJeeth <konerirajb@yahoo.com>
2019-07-19 07:57:37 +0530

    +undid changes in Drive script +Deleted sample scene

--- Assets/Scripts/Drive.cs
+++ Assets/Scripts/Drive.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.UI;
 
 // A very simplistic car driving on the x-z plane.
 
@@ -8,28 +7,9 @@ public class Drive : MonoBehaviour
 {
     public float speed = 10.0f;
     public float rotationSpeed = 100.0f;
-	public Text m_textFuelValue;
 
-	private Vector3 m_vec3CurrentPosition;
-
-	private void Start()
-	{
-
-	}
-
-	void Update()
+    void Update()
     {
-		float fFuelValue = 0; ;
-		if (!string.IsNullOrEmpty(m_textFuelValue.text))
-		{
-			fFuelValue = float.Parse(m_textFuelValue.text);
-		}
-
-		if (fFuelValue <= 0f)
-		{
-			Debug.Log("<color=orange>[Drive] Fuel is empty</color>");
-			return;
-		}
         // Get the horizontal and vertical axis.
         // By default they are mapped to the arrow keys.
         // The value is in the range -1 to 1
@@ -45,11 +25,5 @@ public class Drive : MonoBehaviour
 
         // Rotate around our y-axis
         transform.Rotate(0, 0, -rotation);
-
-		fFuelValue -= Vector3.Distance(transform.position,m_vec3CurrentPosition);
-		m_textFuelValue.text = fFuelValue.ToString();
-
-		m_vec3CurrentPosition = transform.position;
-		Debug.Log("<color=orange>[Drive] The current position has been updated</color>");
-	}
+    }
 }
