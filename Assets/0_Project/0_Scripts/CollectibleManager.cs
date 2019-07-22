6d47be8418391442ec28ef0be490faead1ce62b4
DevJeeth <konerirajb@yahoo.com>
2019-07-22 07:01:11 +0530

    +Done AiController

--- Assets/0_Project/0_Scripts/CollectibleManager.cs
+++ Assets/0_Project/0_Scripts/CollectibleManager.cs
@@ -1,20 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class CollectibleManager : MonoBehaviour
 {
 	[SerializeField] GameObject m_gFuelPrefab;
 	public Vector3 m_vec3FuelPosition;
 
-	void Awake()
+	private Action<Vector3> VectorPosition;
+	public void RegisterToFuelGeneration(Action<Vector3> a_del)
+	{
+		VectorPosition += a_del;
+	}
+
+	public void DeregisterFromFuelGeneration(Action<Vector3> a_del)
+	{
+		if(VectorPosition != null)
+		{
+			VectorPosition -= a_del;
+		}
+	}
+
+	private void Start()
+	{
+		Invoke("GenerateFuel", 5);
+	}
+
+	private void GenerateFuel()
 	{
 		GameObject gFuelPrefab = Instantiate(m_gFuelPrefab, new Vector3(UnityEngine.Random.Range(-100, 100), UnityEngine.Random.Range(-100, 100), -10), Quaternion.identity);
 		m_vec3FuelPosition = gFuelPrefab.transform.position;
+
+		Vector3 Vec2FuelPosition = gFuelPrefab.transform.position;
+		VectorPosition?.DynamicInvoke(Vec2FuelPosition);
 	}
 
 
-    void Update()
+	private void Update()
     {
         
     }
--- Assets/Scripts/Drive.cs
+++ Assets/Scripts/Drive.cs
@@ -5,26 +5,81 @@ using System.Collections;
 
 public class Drive : MonoBehaviour
 {
-    public float speed = 10.0f;
-    public float rotationSpeed = 100.0f;
-	
+    public float m_fspeed = 0.1f;
+    public float m_frotationSpeed = 100.0f;
+	public bool m_bPlayerController = false;
 
 	private Vector2 m_vec2Direction;
+	private Transform m_tFuelPosition;
+	private CollectibleManager m_refCollectibleManager;
+	private Vector3 m_vec3FuelPosition, m_vec3DirVector;
+	private bool m_bFuelFound = false;
 
 	private void Start()
 	{
+		m_refCollectibleManager = FindObjectOfType<CollectibleManager>();
+		if(m_refCollectibleManager == null)
+		{
+			Debug.LogError("[Drive] Could not find CollectibleManager");
+			return;
+		}
 
+		m_refCollectibleManager.RegisterToFuelGeneration(GetFuelPosition);
 	}
 
 	private void Update()
     {
+		if (m_bPlayerController)
+			PlayerDriver();
+		else
+			AiController();
+    }
 
+	private void PlayerDriver()
+	{
 		float fVertical = Input.GetAxis("Vertical");
 		float fHorizontal = Input.GetAxis("Horizontal");
 
 		m_vec2Direction = new Vector2(fHorizontal, fVertical);
 		Vector2 vec2Position = transform.position;
-		vec2Position += m_vec2Direction ;
+		vec2Position += m_vec2Direction;
 		transform.position = vec2Position;
-    }
+	}
+
+	private void AiController()
+	{
+		if(m_bFuelFound)
+		{
+			//if(Vector2.Distance(m_vec2FuelPosition, (Vector2)transform.position) <= 0.45f)
+			//{
+			//	//transform.position = m_vec2DirVector;
+			//	m_bFuelFound = false;
+			//	return;
+			//}
+			//Debug.Log("<color=red>"+ Vector2.Distance(m_vec2FuelPosition, (Vector2)transform.position) + "</color>");
+			transform.position += m_vec3DirVector * m_fspeed * Time.deltaTime;  
+		}
+	}
+
+	private void GetFuelPosition(Vector3 a_vec3FuelPosition)
+	{
+		m_vec3FuelPosition = a_vec3FuelPosition;
+		m_vec3DirVector = transform.position - transform.position;
+		Debug.Log("<color=green>Vector Direction Generated: "+m_vec3DirVector+"</color>");
+
+
+		m_bFuelFound = true;
+		Debug.Log("<color=green>Fuel position found</color>");
+	}
+
+	private void OnDisable()
+	{
+		if (m_refCollectibleManager == null)
+		{
+			Debug.LogError("[Drive] Could not find CollectibleManager");
+			return;
+		}
+
+		m_refCollectibleManager.DeregisterFromFuelGeneration(GetFuelPosition);
+	}
 }
